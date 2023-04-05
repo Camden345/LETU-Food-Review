e2ab9602dcdbffc0347d9bcdf5fd9c5bda1bb84f
Chacko Happy <80079505+ChackoHappy@users.noreply.github.com>
2023-04-05 16:38:45 -0500

    Updates for Class

--- LETU Food Review/LETU Food Review/Form1.Designer.cs
+++ LETU Food Review/LETU Food Review/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace LETU_Food_Review
+﻿using System.Windows.Forms;
+
+namespace LETU_Food_Review
 {
     partial class Form1
     {
@@ -6,19 +8,6 @@
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
 
         #region Windows Form Designer generated code
 
@@ -28,11 +17,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.accountPage1 = new System.Windows.Forms.TabPage();
+            this.account1 = new LETU_Food_Review.Account();
+            this.hivePage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.hive1 = new LETU_Food_Review.Hive();
+            this.sagaPage1 = new System.Windows.Forms.TabPage();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.corner1 = new LETU_Food_Review.Corner();
+            this.homePage1 = new System.Windows.Forms.TabPage();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label6 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -43,74 +40,147 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.account1 = new LETU_Food_Review.Account();
-            this.hive1 = new LETU_Food_Review.Hive();
-            this.corner1 = new LETU_Food_Review.Corner();
-            this.panel2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.accountPage1.SuspendLayout();
+            this.hivePage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.sagaPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.homePage1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // panel2
+            // tabControl1
             // 
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.button1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(0, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(314, 67);
-            this.panel2.TabIndex = 1;
-            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            this.tabControl1.Controls.Add(this.accountPage1);
+            this.tabControl1.Controls.Add(this.hivePage1);
+            this.tabControl1.Controls.Add(this.sagaPage1);
+            this.tabControl1.Controls.Add(this.homePage1);
+            this.tabControl1.ItemSize = new System.Drawing.Size(71, 60);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.Padding = new System.Drawing.Point(15, 3);
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(405, 532);
+            this.tabControl1.TabIndex = 2;
             // 
-            // button4
+            // accountPage1
             // 
-            this.button4.Location = new System.Drawing.Point(252, 17);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(59, 33);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Home";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.accountPage1.Controls.Add(this.account1);
+            this.accountPage1.Location = new System.Drawing.Point(4, 64);
+            this.accountPage1.Name = "accountPage1";
+            this.accountPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.accountPage1.Size = new System.Drawing.Size(397, 464);
+            this.accountPage1.TabIndex = 0;
+            this.accountPage1.Text = "Account";
+            this.accountPage1.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // account1
             // 
-            this.button3.Location = new System.Drawing.Point(13, 17);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(71, 33);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Account";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.account1.Location = new System.Drawing.Point(0, 0);
+            this.account1.Name = "account1";
+            this.account1.Size = new System.Drawing.Size(314, 380);
+            this.account1.TabIndex = 9;
             // 
-            // button2
+            // hivePage1
             // 
-            this.button2.Location = new System.Drawing.Point(171, 22);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Corner";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.hivePage1.Controls.Add(this.pictureBox5);
+            this.hivePage1.Controls.Add(this.pictureBox4);
+            this.hivePage1.Controls.Add(this.hive1);
+            this.hivePage1.Location = new System.Drawing.Point(4, 64);
+            this.hivePage1.Name = "hivePage1";
+            this.hivePage1.Padding = new System.Windows.Forms.Padding(3);
+            this.hivePage1.Size = new System.Drawing.Size(397, 464);
+            this.hivePage1.TabIndex = 1;
+            this.hivePage1.Text = "Hive";
+            this.hivePage1.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // pictureBox5
             // 
-            this.button1.Location = new System.Drawing.Point(90, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Hive";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(144, 85);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 52);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(20, 85);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 52);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            // 
+            // hive1
+            // 
+            this.hive1.Location = new System.Drawing.Point(3, 0);
+            this.hive1.Name = "hive1";
+            this.hive1.Size = new System.Drawing.Size(314, 380);
+            this.hive1.TabIndex = 10;
+            this.hive1.Load += new System.EventHandler(this.hive1_Load);
+            // 
+            // sagaPage1
+            // 
+            this.sagaPage1.Controls.Add(this.pictureBox7);
+            this.sagaPage1.Controls.Add(this.pictureBox6);
+            this.sagaPage1.Controls.Add(this.corner1);
+            this.sagaPage1.Location = new System.Drawing.Point(4, 64);
+            this.sagaPage1.Name = "sagaPage1";
+            this.sagaPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.sagaPage1.Size = new System.Drawing.Size(397, 464);
+            this.sagaPage1.TabIndex = 2;
+            this.sagaPage1.Text = "Corner";
+            this.sagaPage1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox7.Image")));
+            this.pictureBox7.Location = new System.Drawing.Point(152, 91);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(88, 61);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 13;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(31, 91);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(88, 61);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 12;
+            this.pictureBox6.TabStop = false;
+            // 
+            // corner1
+            // 
+            this.corner1.Location = new System.Drawing.Point(0, 0);
+            this.corner1.Name = "corner1";
+            this.corner1.Size = new System.Drawing.Size(314, 382);
+            this.corner1.TabIndex = 11;
+            // 
+            // homePage1
+            // 
+            this.homePage1.Controls.Add(this.panel1);
+            this.homePage1.Location = new System.Drawing.Point(4, 64);
+            this.homePage1.Name = "homePage1";
+            this.homePage1.Padding = new System.Windows.Forms.Padding(3);
+            this.homePage1.Size = new System.Drawing.Size(397, 464);
+            this.homePage1.TabIndex = 3;
+            this.homePage1.Text = "Home";
+            this.homePage1.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.corner1);
-            this.panel1.Controls.Add(this.hive1);
-            this.panel1.Controls.Add(this.account1);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.pictureBox3);
             this.panel1.Controls.Add(this.label5);
@@ -120,7 +190,7 @@
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(0, 69);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(314, 380);
             this.panel1.TabIndex = 0;
@@ -136,9 +206,11 @@
             // 
             // pictureBox3
             // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
             this.pictureBox3.Location = new System.Drawing.Point(13, 187);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(131, 76);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 7;
             this.pictureBox3.TabStop = false;
             // 
@@ -180,17 +252,21 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(171, 78);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(131, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(13, 78);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(131, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
@@ -203,41 +279,26 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Reviewed Foods";
             // 
-            // account1
-            // 
-            this.account1.Location = new System.Drawing.Point(0, 0);
-            this.account1.Name = "account1";
-            this.account1.Size = new System.Drawing.Size(314, 380);
-            this.account1.TabIndex = 9;
-            this.account1.Load += new System.EventHandler(this.account1_Load);
-            // 
-            // hive1
-            // 
-            this.hive1.Location = new System.Drawing.Point(0, 0);
-            this.hive1.Name = "hive1";
-            this.hive1.Size = new System.Drawing.Size(314, 380);
-            this.hive1.TabIndex = 10;
-            // 
-            // corner1
-            // 
-            this.corner1.Location = new System.Drawing.Point(0, -2);
-            this.corner1.Name = "corner1";
-            this.corner1.Size = new System.Drawing.Size(314, 382);
-            this.corner1.TabIndex = 11;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(314, 449);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(403, 530);
+            this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.accountPage1.ResumeLayout(false);
+            this.hivePage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.sagaPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.homePage1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -248,12 +309,7 @@
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Label label5;
@@ -266,6 +322,15 @@
         private Account account1;
         private Corner corner1;
         private Hive hive1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage accountPage1;
+        private System.Windows.Forms.TabPage hivePage1;
+        private System.Windows.Forms.TabPage sagaPage1;
+        private System.Windows.Forms.TabPage homePage1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
 
--- LETU Food Review/LETU Food Review/Form1.cs
+++ LETU Food Review/LETU Food Review/Form1.cs
@@ -17,55 +17,18 @@ namespace LETU_Food_Review
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            corner1.Show();
-            corner1.BringToFront();
-
-            hive1.Hide();
-            account1.Hide();
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            hive1.Hide();
-            corner1.Hide();
-            account1.Hide();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void hive1_Load(object sender, EventArgs e)
         {
-            hive1.Show();
-            hive1.BringToFront();
 
-            corner1.Hide();
-            account1.Hide();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            account1.Show();
-            account1.BringToFront();
-
-            corner1.Hide();
-            hive1.Hide();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            hive1.Hide();
-            corner1.Hide();
-            account1.Hide();
-        }
-
-        private void account1_Load(object sender, EventArgs e)
-        {
-
+            Console.WriteLine(e.ColumnIndex.ToString(), sender.ToString());
         }
     }
 }
